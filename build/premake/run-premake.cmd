ac01a50fd0851d505387d4b515ea6101741e48a3
Doug Hodson <doug@sidechannel.net>
2019-06-10 21:00:24 -0400

    updated premake to alpha14

--- build/premake/run-premake.cmd
+++ build/premake/run-premake.cmd
@@ -2,6 +2,7 @@ rem delete old solution/workspace files
 rem
 rmdir /S /Q ..\gmake
 rmdir /S /Q ..\vs2017
+rmdir /S /Q ..\vs2019
 rmdir /S /Q ..\obj
 rem
 rem delete old compiled libraries
@@ -10,4 +11,5 @@ del ..\..\lib\*.lib
 rem
 rem create new solution/workspace files
 rem
-premake5-alpha13.exe --os=windows --file=premake5.lua vs2017
+premake5-alpha14.exe --os=windows --file=premake5.lua vs2017
+premake5-alpha14.exe --os=windows --file=premake5.lua vs2019
--- build/premake/run-premake.sh
+++ build/premake/run-premake.sh
@@ -3,6 +3,7 @@
 #
 rm -rf ..\gmake
 rm -rf ..\vs2017
+rm -rf ..\vs2019
 rm -rf ..\obj
 #
 # remove any compiled libraries
@@ -11,4 +12,5 @@ rm -rf ..\..\lib\*.lib
 #
 # create new build files files
 #
-./premake5-alpha13 --os=linux --file=premake5.lua gmake
+./premake5-alpha14 --os=linux --file=premake5.lua gmake
+
