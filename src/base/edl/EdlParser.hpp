52b0ac6fbb4a8bed47042367216ec7e0905037ed
Doug Hodson <doug@sidechannel.net>
2023-10-12 16:43:44 -0400

    updated bison

--- src/base/edl/EdlParser.hpp
+++ src/base/edl/EdlParser.hpp
@@ -1,8 +1,9 @@
-/* A Bison parser, made by GNU Bison 3.2.2.  */
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -30,8 +31,9 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
 
 #ifndef YY_YY_EDLPARSER_HPP_INCLUDED
 # define YY_YY_EDLPARSER_HPP_INCLUDED
@@ -43,26 +45,30 @@
 extern int yydebug;
 #endif
 
-/* Token type.  */
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    IDENT = 258,
-    SLOT_ID = 259,
-    INTEGERconstant = 260,
-    FLOATINGconstant = 261,
-    BOOLconstant = 262,
-    STRING_LITERAL = 263
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    IDENT = 258,                   /* IDENT  */
+    SLOT_ID = 259,                 /* SLOT_ID  */
+    INTEGERconstant = 260,         /* INTEGERconstant  */
+    FLOATINGconstant = 261,        /* FLOATINGconstant  */
+    BOOLconstant = 262,            /* BOOLconstant  */
+    STRING_LITERAL = 263           /* STRING_LITERAL  */
   };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
 union YYSTYPE
 {
-#line 110 "edl_parser.y" /* yacc.c:1906  */
+#line 110 "edl_parser.y"
 
    double                     dval;
    long                       lval;
@@ -74,9 +80,9 @@ union YYSTYPE
    mixr::base::List*          lvalp;
    mixr::base::Number*        nvalp;
 
-#line 78 "EdlParser.hpp" /* yacc.c:1906  */
-};
+#line 84 "EdlParser.hpp"
 
+};
 typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
@@ -85,6 +91,8 @@ typedef union YYSTYPE YYSTYPE;
 
 extern YYSTYPE yylval;
 
+
 int yyparse (void);
+
 
 #endif /* !YY_YY_EDLPARSER_HPP_INCLUDED  */
