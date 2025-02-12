0d73865fdd08c0b0f913558d2961dde3e994a789
Doug Hodson <doug@sidechannel.net>
2025-02-12 08:57:46 -0500

    added more FTGL fonts to form func

--- src/graphics/fonts/ftgl/factory.cpp
+++ src/graphics/fonts/ftgl/factory.cpp
@@ -5,6 +5,11 @@
 // ftgl fonts
 #include "mixr/graphics/fonts/ftgl/FtglBitmapFont.hpp"
 #include "mixr/graphics/fonts/ftgl/FtglExtrdFont.hpp"
+#include "mixr/graphics/fonts/ftgl/FtglHaloFont.hpp"
+#include "mixr/graphics/fonts/ftgl/FtglOutlineFont.hpp"
+#include "mixr/graphics/fonts/ftgl/FtglPixmapFont.hpp"
+#include "mixr/graphics/fonts/ftgl/FtglPolygonFont.hpp"
+#include "mixr/graphics/fonts/ftgl/FtglTextureFont.hpp"
 
 #include <string>
 
@@ -23,6 +28,24 @@ base::Object* factory(const std::string& name)
    else if ( name == FtglExtrdFont::getFactoryName() ) {
       obj = new FtglExtrdFont();
    }
+   else if ( name == FtglHaloFont::getFactoryName() ) {
+      obj = new FtglHaloFont();
+   }
+   else if ( name == FtglOutlineFont::getFactoryName() ) {
+      obj = new FtglOutlineFont();
+   }
+   else if ( name == FtglPixmapFont::getFactoryName() ) {
+      obj = new FtglPixmapFont();
+   }
+   else if ( name == FtglPolygonFont::getFactoryName() ) {
+      obj = new FtglPolygonFont();
+   }
+   else if ( name == FtglTextureFont::getFactoryName() ) {
+      obj = new FtglTextureFont();
+   }
+
+
+
 
    return obj;
 }
