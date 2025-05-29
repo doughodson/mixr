61dcc3b5f72ae1c49c39583b37f8d365f752d431
Doug Hodson <doug@sidechannel.net>
2025-05-29 09:31:35 -0400

    added concrete list class

---
+++ include/mixr/base/List.hpp
@@ -0,0 +1,27 @@
+
+#ifndef __mixr_base_List_HPP__
+#define __mixr_base_List_HPP__
+
+#include "mixr/base/IList.hpp"
+
+namespace mixr {
+namespace base {
+
+//------------------------------------------------------------------------------
+// Class: List
+// Description: Concrete list class.
+//
+// Factory name: List
+//------------------------------------------------------------------------------
+class List final: public IList
+{
+   DECLARE_SUBCLASS(List, IList)
+
+public:
+   List();
+};
+
+}
+}
+
+#endif
---
+++ src/base/List.cpp
@@ -0,0 +1,14 @@
+#include "mixr/base/List.hpp"
+
+namespace mixr {
+namespace base {
+
+IMPLEMENT_SUBCLASS(List, "List")
+EMPTY_SLOTTABLE(List)
+EMPTY_COPYDATA(List)
+EMPTY_DELETEDATA(List)
+EMPTY_CONSTRUCTOR(List)
+
+}
+}
+
